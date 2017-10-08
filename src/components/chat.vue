5e58f9f61ca050425eb8b09a202abc3a9ca32112
expertep <esred@hotmail.com>
2017-10-08 17:32:06 +0700

    firebase

--- src/components/chat.vue
+++ src/components/chat.vue
@@ -6,7 +6,9 @@
             Friends
           </p>
           <ul class="menu-list">
-            <li><a>แหนม</a></li>
+            <li v-for="user in users" class="user" :key="user['.key']">
+              <span>{{user.username}}</span>
+            </li>
           </ul>
         </aside>
       </div>
@@ -19,25 +21,42 @@
             <textarea class="textarea" placeholder="Textarea"></textarea>
           </div>
           <div class="control">
-            <button class="button is-primary">Submit</button>
+            <button class="button is-primary">sent</button>
           </div>
         </div>
 
       </div>
       <div class="notepanel column is-4 is-hidden-mobile">
         <h1>note</h1>
-        {{username}}
+        <div v-for="doc in note" class="box" :key="doc['.key']">
+          {{doc.text}}
+        </div>
       </div>
   </div>
 </template>
 
 <script>
+var firebase = require('firebase')
+
+var config = {
+  apiKey: 'AIzaSyD4lqtwAYmjp1B7orDSsztnOxhYAhQfFAk',
+  authDomain: 'chat-fda99.firebaseapp.com',
+  databaseURL: 'https://chat-fda99.firebaseio.com',
+  storageBucket: 'chat-fda99.appspot.com'
+}
+firebase.initializeApp(config)
+var usersRef = firebase.database().ref('users')
+var noteRef = firebase.database().ref('note')
 export default {
   name: 'chat',
   data () {
     return {
       username: 's'
     }
+  },
+  firebase: {
+    users: usersRef,
+    note: noteRef
   }
 }
 </script>
@@ -46,6 +65,7 @@ export default {
 <style scoped>
   .friendspanel {
     background-color: rgb(55,55,55);
+    color : white;
   }
   .chatpanel {
     color : white;
@@ -56,7 +76,5 @@ export default {
     background-color: rgb(46,46,46);
     border-left: 2px solid rgb(69, 69, 69);
   }
-  .main {
-    height : 50em;
-  }
+
 </style>
--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import VueFire from 'vuefire'
+
 Vue.use(VueFire)
 
 require('./bulma/css/bulma.css')
